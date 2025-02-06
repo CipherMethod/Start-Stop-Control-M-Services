08dd9edf768476a334f4ca6e9156171185e4a3fa
Jamey Hopkins <35248862+CipherMethod@users.noreply.github.com>
2025-02-06 13:51:34 -0700

    30 second note for full shutdown

--- stop-control-m.sh
+++ stop-control-m.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Stop Control-M Services
 #
-# 20240207 jah - developed and tested on 9.0.21.200, 9.0.21.300
+# 20240207 jah - developed and tested on 9.0.21.200
 # 20241017 jah - single control-m user/directory conversion
 # 20250206 jah - change em -M option to -all
 
@@ -12,5 +12,7 @@ su - control-m -c "shut_ctm"
 su - control-m -c "shut_ca"
 su - control-m -c "shut-ag -u control-m -p ALL"
 su - control-m -c "shutdb"
+echo
+echo "NOTE: It may take an additional 30 seconds for services to fully shutdown."
 echo
 
